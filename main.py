74995a64542b5c244767cec791b5c0e386e763a1
Forrest Zhang <yunyuzhang11@gmail.com>
2023-04-09 16:45:53 -0400

    Added ability to connect to firebase and uploaded faked data

--- main.py
+++ main.py
@@ -1,19 +1,67 @@
 import os
 from flask import Flask, jsonify, request
-app = Flask(__name__)
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
+import json
 
 
-testJson = [
-    { 'name': 'Forrest', 'amount': 5000 }
-]
+class WebServer:
+    def __init__(self):
+        self.app = Flask(__name__)
 
-@app.route("/test")
-def main():
-    return jsonify(testJson)
+        self.testJson = [
+            { 'name': 'Forrest', 'amount': 5000 }
+        ]
 
-@app.route('/')
-def hello():
-    return 'Welcome'
+        self.generateURL()
+        self.initFirebase()
+        # self.fbtest()
+
+       
+    def generateURL(self):
+        self.app.route("/test")(self.main)
+        self.app.route('/')(self.hello)
+        self.app.route('/weight', methods=['GET'])(self.receive_weight)
+        self.app.route('/usage', methods=['GET'])(self.receive_usage)
+
+    def initFirebase(self):
+        cred = credentials.Certificate("config/iotprojectbackendtest-firebase-adminsdk-rzhc2-80855871ed.json")
+
+        default_app = firebase_admin.initialize_app(cred, {
+            'databaseURL': "https://iotprojectbackendtest-default-rtdb.firebaseio.com/"
+            })
+        ref = db.reference("/")
+        with open("testData.json", "r") as f:
+            content = json.load(f)
+        ref.set(content)
+        print(ref.get())
+        
+
+    def main(self):
+        return jsonify(self.testJson)
+
+    def hello(self):
+        return 'Welcome'
+
+    def receive_weight(self):
+        weight = request.args.get('value')
+        timestamp = request.args.get('timestamp')
+        catName = request.args.get('catName')
+        return f'Current weight: {weight}, {timestamp}, {catName}'
+
+    def receive_usage(self):
+        timestamp = request.args.get('timestamp')
+        cat_name = request.args.get('catName')
+        usageType = request.args.get('usageType')
+        return f'{timestamp}, {cat_name}, {usageType}'
+
+    def upload_to_firebase(self):
+        return
+
+    def run(self):
+        self.app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+    my_app = WebServer()
+    my_app.run()
