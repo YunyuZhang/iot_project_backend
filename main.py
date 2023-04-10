c47a97cb0a61fa15023ae5abacb7b9d2af879658
Forrest Zhang <yunyuzhang11@gmail.com>
2023-04-09 23:28:18 -0400

    Both weight and usage now write to DB and added test data

--- main.py
+++ main.py
@@ -4,26 +4,27 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import db
 import json
+from enum import Enum
 
+
+class LogType(Enum):
+    WEIGHT = "WeightHistory"
+    USAGE = "UsageHistory"
 
 class WebServer:
     def __init__(self):
         self.app = Flask(__name__)
 
-        self.userID = "yunyuzhang11"
-
         self.testJson = [
-            { 'name': 'Forrest', 'amount': 5000 }
+            { 'name': 'Forrest', 'message': "Server is running" }
         ]
-
         self.generateURL()
         self.initFirebase()
-        self.upload_to_firebase()
+        self.populateData()
 
        
     def generateURL(self):
-        self.app.route("/test")(self.main)
-        self.app.route('/')(self.hello)
+        self.app.route("/")(self.test)
         self.app.route('/weight', methods=['GET'])(self.receive_weight)
         self.app.route('/usage', methods=['GET'])(self.receive_usage)
 
@@ -32,42 +33,46 @@ class WebServer:
 
         default_app = firebase_admin.initialize_app(cred, {
             'databaseURL': "https://iotprojectbackendtest-default-rtdb.firebaseio.com/"
-            })
+        })
+    
+    
+    def populateData(self):
         ref = db.reference("/")
         with open("testData.json", "r") as f:
             content = json.load(f)
         ref.set(content)
         print(ref.get())
+
         
 
-    def main(self):
+    def test(self):
         return jsonify(self.testJson)
 
-    def hello(self):
-        return 'Welcome'
-
     def receive_weight(self):
-        weight = request.args.get('value')
-        timestamp = request.args.get('timestamp')
-        catName = request.args.get('catName')
-
-        return f'Current weight: {weight}, {timestamp}, {catName}'
+        
+        dataDict = {
+            "weightValue": request.args.get('value'),
+            "timestamp": request.args.get('timestamp'),
+            "catName": request.args.get('catName'),
+        }
+        userID =  request.args.get('userID')
+        self.upload_to_firebase(LogType.WEIGHT, dataDict, userID)
+        return dataDict
 
     def receive_usage(self):
-        timestamp = request.args.get('timestamp')
-        cat_name = request.args.get('catName')
-        usageType = request.args.get('usageType')
-        return f'{timestamp}, {cat_name}, {usageType}'
+        dataDict = {
+            "usageType": request.args.get('usageType'),
+            "timestamp": request.args.get('timestamp'),
+            "catName": request.args.get('catName'),
+        }
+        userID =  request.args.get('userID')
+        self.upload_to_firebase(LogType.USAGE, dataDict, userID)
+        return dataDict
 
-    def upload_to_firebase(self):
-        newData = {
-                    "timestamp": "2022-07-16 11:11:11",
-                    "eventType": "poop",
-                    "catName": "Haybe"
-                }
-        ref = db.reference("/" + self.userID).child("UsageHistory")
-        ref.child("2022-07-16 11:11:11").set(newData)
-        return
+    def upload_to_firebase(self, logType, dataDict, userID):
+        ref = db.reference("/" + userID).child(logType.value)
+        ref.child(dataDict["timestamp"]).set(dataDict)
+        return logType.value + " uploaded to DB"
 
     def run(self):
         self.app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
